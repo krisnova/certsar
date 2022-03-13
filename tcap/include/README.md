3c28e6bbb72ee966583858a80e1ab97d3fe4bca7
Kris Nóva <kris@nivenly.com>
2022-03-13 13:18:05 -0400

    adding tcap scaffolding
    Signed-off-by: Kris Nóva <kris@nivenly.com>

---
+++ tcap/include/README.md
@@ -0,0 +1,6 @@
+# TCap API
+
+The `tcap.h` file is the "public API" we expose for the shared object library.
+
+This will be the main interface (and backwards compatability) for the shared object.
+
---
+++ tcap/tlsdump/tlsdump.c
@@ -0,0 +1,75 @@
+// Copyright © 2022 The Certsar Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//   ██████╗███████╗██████╗ ████████╗███████╗ █████╗ ██████╗
+//  ██╔════╝██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗
+//  ██║     █████╗  ██████╔╝   ██║   ███████╗███████║██████╔╝
+//  ██║     ██╔══╝  ██╔══██╗   ██║   ╚════██║██╔══██║██╔══██╗
+//  ╚██████╗███████╗██║  ██║   ██║   ███████║██║  ██║██║  ██║
+//   ╚═════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
+
+// Include the global tcap header
+#include "tcap.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void usage() {
+    // TODO add about() in the library!
+    //about();
+    printf("\n");
+    printf("Usage:\n");
+    printf("tlsdump [options...] \n");
+    printf("\n");
+    printf("[Options]\n");
+    printf("\n");
+    printf("  -h               print the usage and help screen.\n");
+    printf("\n");
+}
+
+
+// main
+//
+// Main entrypoint for tlsdump
+//
+int main (int argc, char **argv) {
+
+    int help = 0;
+    int option;
+
+    while (optind < argc) {
+        if ((option = getopt(argc, argv, "hjrx")) != -1) {
+            switch (option) {
+                case 'h':
+                    usage();
+                    return 1;
+                default:
+                    usage();
+                    return 1;
+            }
+        } else {
+            // This is the system that will handle
+            // command line values other than flags
+            // optind = 0 : tlsdump
+            // optind = 1 : <input>
+            if (optind == 1) {
+                // Parse input
+            }
+        }
+        optind++;
+    }
+
+    printf("[tlsdump]: \n");
+
+}
